a6df602745b8ec3f3d5ae01aad3657142d618014
Jakub Schenk <jakubschenk3@gmail.com>
2023-01-21 20:49:22 +0100

    feat: Try withAuth middleware

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,8 @@
 import { withAuth } from "next-auth/middleware";
-import { NextRequest, NextResponse } from "next/server";
 
 export default withAuth({
   callbacks: {
-    authorized({ req, token }) {
+    authorized({ token }) {
       console.log(token);
       return !!token;
     },
